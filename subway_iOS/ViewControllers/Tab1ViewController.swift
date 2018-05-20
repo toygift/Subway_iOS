e425c5b68048f36639d1bf107f0b0403667edf71
gyugyu90 <kh2000park@gmail.com>
2018-05-20 16:08:26 +0900

    separate dev prod

--- subway_iOS/ViewControllers/Tab1ViewController.swift
+++ subway_iOS/ViewControllers/Tab1ViewController.swift
@@ -13,6 +13,17 @@ class Tab1ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("tab1")
+        
+//        if let infoDic = Bundle.main.infoDictionary {
+//            let currentBuildSetting = infoDic["CURRENT_BUILD_SETTING"] as! String
+//            print(currentBuildSetting)
+//        }
+        
+        if let currentBuild = Bundle.main.object(forInfoDictionaryKey: "CurrentBuildSetting") as? String {
+            print(currentBuild)
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
 
