5596b5890a723cb858d2e8b83737e10abbd708c0
gyugyu90 <kh2000park@gmail.com>
2018-05-20 15:44:58 +0900

    setting project structure

---
+++ subway_iOS/ViewControllers/Tab3ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Tab3ViewController.swift
+//  subway_iOS
+//
+//  Created by khpark on 2018. 5. 20..
+//  Copyright © 2018년 TeamSubway. All rights reserved.
+//
+
+import UIKit
+
+class Tab3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("tab3")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
