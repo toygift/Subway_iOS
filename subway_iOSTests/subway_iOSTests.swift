874497088fc9bb30d053fa5281e2b3959957d188
gyugyu90 <kh2000park@gmail.com>
2018-05-20 15:15:37 +0900

    Initial Commit

---
+++ subway_iOSTests/subway_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  subway_iOSTests.swift
+//  subway_iOSTests
+//
+//  Created by khpark on 2018. 5. 20..
+//  Copyright © 2018년 TeamSubway. All rights reserved.
+//
+
+import XCTest
+@testable import subway_iOS
+
+class subway_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
