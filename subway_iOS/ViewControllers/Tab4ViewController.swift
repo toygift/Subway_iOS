d7c2b91dc2376007fb0dcc61d43b28c1394657f9
gyugyu90 <kh2000park@gmail.com>
2018-10-21 15:54:26 +0900

    add template for menu tab

--- subway_iOS/ViewControllers/Tab4ViewController.swift
+++ subway_iOS/ViewControllers/Tab4ViewController.swift
@@ -8,31 +8,30 @@
 
 import UIKit
 
-enum MemuList: String {
-    case name = "이름"
-    case setting = "설정"
-    
-    static func getList() -> [String] {
-        return [MemuList.name.rawValue, MemuList.setting.rawValue]
-    }
-    static func getLists() -> [MemuList] {
-        return [.name, .setting]
-    }
+struct Setting {
+    var title = ""
+    var imageName = ""
 }
-
 
 class Tab4ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    @IBAction func logout(_ sender: UIBarButtonItem) {
-        TokenAuth.delete(serviceName, account: TokenAuth.SERVER_TOKEN)
-        goToAuth()
-    }
+//    @IBAction func logout(_ sender: UIBarButtonItem) {
+//        TokenAuth.delete(serviceName, account: TokenAuth.SERVER_TOKEN)
+//        goToAuth()
+//    }
+    
+    var settingList = [Setting(title: "공지사항", imageName: "clipboard"),
+                       Setting(title: "원산지 정보", imageName: "compass"),
+                       Setting(title: "알레르기 정보", imageName: "pill"),
+                       Setting(title: "로그아웃", imageName: "logOut"),
+                       Setting(title: "회원탈퇴", imageName: "userX")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        tableView.rowHeight = 55
     }
     
     fileprivate func goToAuth(){
@@ -44,32 +43,32 @@ class Tab4ViewController: UIViewController {
 }
 extension Tab4ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return MemuList.getList().count
+        return settingList.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as? SettingTableViewCell {
-            cell.setting(with: MemuList.getList()[indexPath.row])
+            cell.setting(with: settingList[indexPath.row])
             return cell
         }
         return UITableViewCell()
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch MemuList.getLists()[indexPath.row] {
-        case .name:
-            print("name")
-        case .setting:
-            print("setting")
-        }
+        let title = settingList[indexPath.row].title
+        print(title)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
+    
 }
 
 
 class SettingTableViewCell: UITableViewCell {
     static let identifier = "MENUCELL"
     
+    @IBOutlet weak var logoIV: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     
-    func setting(with data: String) {
-        self.titleLabel.text = data
+    func setting(with data: Setting) {
+        self.titleLabel.text = data.title
+        self.logoIV.image = UIImage(named: data.imageName)
     }
 }
